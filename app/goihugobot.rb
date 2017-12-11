16ec8cdfb30cb19272301fffc68f04fec6c5c9e8
FeNtok\fenrirtok <s14t269@stmail.eng.kagawa-u.ac.jp>
2017-12-11 19:54:35 +0900

    test
    testtt

--- app/goihugobot.rb
+++ app/goihugobot.rb
@@ -1,14 +1,14 @@
-TOKEN = 'uVjsQdmxZbgrkceqMPVaowWXiIV8zKa'
+TOKEN = 'Mzg5NjAzOTk0NjMwMjI1OTIw.DQ_vRw.ghrwtSPzW6fxT19t6I54-XrucMc'
 CLIENT_ID = 389603994630225920
 TOKEN.freeze
 CLIENT_ID.freeze
 
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: TOKEN, client_id: CLIENT_ID
+bot = Discordrb::Commands::CommandBot.new token:TOKEN, client_id: CLIENT_ID, prefix: '/'
 
-bot.message do |event|
-    event.respond 'Hello, world!'
+bot.command :hello do |event|
+ event.send_message("hallo,world.#{event.user.name}")
 end
 
 bot.run
