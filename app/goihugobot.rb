6ad418c5f737f084b4b36c206a7fe48d67f500d4
FeNtok\fenrirtok <s14t269@stmail.eng.kagawa-u.ac.jp>
2017-12-13 22:38:13 +0900

    いろいろする

--- app/goihugobot.rb
+++ app/goihugobot.rb
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-TOKEN = 'uVjsQdmxZbgrkceqMPVaowWXiIV8zKa'
->>>>>>> parent of 16ec8cd... test
-=======
-TOKEN = 'Mzg5NjAzOTk0NjMwMjI1OTIw.DQ_vRw.ghrwtSPzW6fxT19t6I54-XrucMc'
->>>>>>> parent of c43409a... いろいろ
 CLIENT_ID = 389603994630225920
 TOKEN.freeze
 CLIENT_ID.freeze
@@ -14,14 +6,7 @@ CLIENT_ID.freeze
 #https://discordapp.com/api/oauth2/authorize?client_id=389603994630225920&permissions=75776&scope=bot
 
 require 'discordrb'
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 bot = Discordrb::Commands::CommandBot.new token:ENV["TOKEN"], client_id: CLIENT_ID, prefix: '/'
-=======
-bot = Discordrb::Commands::CommandBot.new token:TOKEN, client_id: CLIENT_ID, prefix: '/'
->>>>>>> parent of c43409a... いろいろ
-
 bot.command :hello do |event|
  event.send_message("こんにちは。#{event.user.name}")
 end
@@ -35,19 +20,10 @@ end
 # 語彙リストを表示
 #bot.command :list do |event|
 #  File.open(../goi/{event.userame}.txt)
-<<<<<<< HEAD
-  event.send_message("#{event.user.name}の語彙リストです。(まだ未実装です)")
-=======
-bot = Discordrb::Bot.new token: TOKEN, client_id: CLIENT_ID
 
-bot.message do |event|
-    event.respond 'Hello, world!'
->>>>>>> parent of 16ec8cd... test
-end
-=======
 #  event.end_message()
 #end
->>>>>>> parent of c43409a... いろいろ
+
 
 # 語彙リストの中から番号に対応した語彙を表示
 
